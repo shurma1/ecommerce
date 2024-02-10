164f8901fe5364cbd8de55d354714ce5f05158b5
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-02-10 17:33:23 +0300

    fix paths

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,9 @@
 import {type ResolveOptions} from 'webpack';
+import {TsconfigPathsPlugin} from 'tsconfig-paths-webpack-plugin';
 
 export const buildResolvers = (): ResolveOptions => ({
-	extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [
+        new TsconfigPathsPlugin()
+    ]
 });
